9cabbfd40f96cfc59541cc73246521dd82efba8f
Rexios <rexios@rexios.dev>
2024-01-06 13:05:28 +0100

    Add tests

--- example/kagi_api_example.dart
+++ example/kagi_api_example.dart
@@ -3,15 +3,13 @@ import 'dart:io';
 import 'package:kagi_api/kagi_api.dart';
 
 void main() async {
-  final token = File('example/secret').readAsStringSync();
+  final token = File('secret/secret').readAsStringSync();
   final kagi = Kagi(token: token);
   final answer = await kagi.fastGpt(query: 'What is the meaning of life?');
   print(answer.data?.output);
 
-  final summary = await kagi.summarize(
-    url:
-        'https://www.politico.eu/article/hydroxychloroquine-could-have-caused-17000-deaths-during-covid-study-finds/',
-  );
+  final summary =
+      await kagi.summarize(url: 'https://pub.dev/packages/rexios_lints');
   print(summary.data?.output);
 
   final enriched = await kagi.enrich(query: 'What is the meaning of life?');
--- test/kagi_api_test.dart
+++ test/kagi_api_test.dart
@@ -1,16 +1,34 @@
+import 'dart:io';
+
 import 'package:kagi_api/kagi_api.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('A group of tests', () {
-    final awesome = Awesome();
+  final token = File('secret/secret').readAsStringSync();
+  final kagi = Kagi(token: token);
 
-    setUp(() {
-      // Additional setup goes here.
-    });
+  test('summarize', () async {
+    final summary =
+        await kagi.summarize(url: 'https://pub.dev/packages/rexios_lints');
+    expect(summary.data?.output, isNotEmpty);
+  });
 
-    test('First Test', () {
-      expect(awesome.isAwesome, isTrue);
-    });
+  test('fastGpt', () async {
+    final answer = await kagi.fastGpt(query: 'What is the meaning of life?');
+    expect(answer.data?.output, isNotEmpty);
+  });
+
+  test(
+    'search',
+    () async {
+      final search = await kagi.search(query: 'What is the meaning of life?');
+      expect(search.data?.results, isNotEmpty);
+    },
+    skip: 'Invite only API',
+  );
+
+  test('enrich', () async {
+    final enriched = await kagi.enrich(query: 'What is the meaning of life?');
+    expect(enriched.data?.results, isNotEmpty);
   });
 }
